9c801d96a4f61bd9a908f263a4094ca8e5e47bd6
Gabriel Ittner <gabriel.ittner@gmail.com>
2019-05-04 12:23:51 +0200

    add FlowPreview and UserExperimental annotations

--- library/src/main/kotlin/com/freeletics/flowredux/SideEffect.kt
+++ library/src/main/kotlin/com/freeletics/flowredux/SideEffect.kt
@@ -1,19 +1,24 @@
 package com.freeletics.flowredux
 
+import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.flow.Flow
 
 /**
  * It is a function which takes a stream of actions and returns a stream of actions. Actions in, actions out
  * (concept borrowed from redux-observable.js.or - so called epics).
+ *
  * @param actions Input action. Every SideEffect should be responsible to handle a single Action
  * (i.e using filter or ofType operator)
  * @param state [StateAccessor] to get the latest state of the state machine
  */
+// @FlowPreview would be nicer because it's viral and let's the user know that this is using an
+// experimental API, but can't be used on a typealias. It's ok though because reduxStore has it.
+@UseExperimental(FlowPreview::class)
 // TODO find better name?
 typealias SideEffect<S, A> = (actions: Flow<A>, state: StateAccessor<S>) -> Flow<A>
 
 /**
- * The StateAccessor is basically just a deferred way to get a state of a [ObservableReduxStore] at any given point in time.
+ * The StateAccessor is basically just a deferred way to get a state of a [reduxStore] at any given point in time.
  * So you have to call this method to get the state.
  */
 // TODO find better name
