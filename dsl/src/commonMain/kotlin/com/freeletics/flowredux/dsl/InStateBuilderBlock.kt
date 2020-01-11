39b1383faa78b5b1768750662c27e28de4fd99f5
Hannes Dorfmann <hannes.dorfmann@gmail.com>
2020-01-11 16:18:05 +0100

    Moved OnActionSideEffectBuilder in own file

--- dsl/src/commonMain/kotlin/com/freeletics/flowredux/dsl/InStateBuilderBlock.kt
+++ dsl/src/commonMain/kotlin/com/freeletics/flowredux/dsl/InStateBuilderBlock.kt
@@ -3,12 +3,10 @@ package com.freeletics.flowredux.dsl
 import com.freeletics.flowredux.SideEffect
 import com.freeletics.flowredux.StateAccessor
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.flatMapConcat
 import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.flatMapMerge
 import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.map
 import kotlin.reflect.KClass
 
 // TODO @DslMarker
@@ -25,7 +23,7 @@ class InStateBuilderBlock<S : Any, SubState : S, A : Any>(
     ) {
 
         @Suppress("UNCHECKED_CAST")
-        val builder = OnActionSideEffectBuilder<S, A, SubState>(
+        val builder = OnActionInStateSideEffectBuilder<S, A, SubState>(
             flatMapPolicy = flatMapPolicy,
             subActionClass = SubAction::class,
             subStateClass = _subStateClass,
@@ -81,76 +79,6 @@ class InStateBuilderBlock<S : Any, SubState : S, A : Any>(
  */
 abstract class InStateSideEffectBuilder<S, A> internal constructor() {
     internal abstract fun generateSideEffect(): SideEffect<S, Action<S, A>>
-}
-
-class OnActionSideEffectBuilder<S : Any, A : Any, SubState : S>(
-    private val subStateClass: KClass<SubState>,
-    internal val subActionClass: KClass<out A>,
-    internal val flatMapPolicy: FlatMapPolicy,
-    internal val onActionBlock: OnActionBlock<S, A>
-) : InStateSideEffectBuilder<S, A>() {
-
-    /**
-     * Creates a Flow that filters for a given (sub)state and (sub)action and returns a
-     * Flow of (sub)action
-     */
-    private fun Flow<Action<S, out A>>.filterStateAndUnwrapExternalAction(
-        stateAccessor: StateAccessor<S>,
-        subStateClass: KClass<out S>
-    ): Flow<A> =
-        this
-            .filter { action ->
-                val conditionHolds = subStateClass.isInstance(stateAccessor()) &&
-                    action is ExternalWrappedAction &&
-                    subActionClass.isInstance(action.action)
-
-                println("filter $action $conditionHolds")
-
-                conditionHolds
-
-            }.map {
-                when (it) {
-                    is ExternalWrappedAction<*, *> -> it.action as A // subActionClass.cast(it.action) // TODO kotlin native supported?
-                    is SelfReducableAction -> throw IllegalArgumentException("Internal bug. Please file an issue on Github")
-                    is InitialStateAction -> throw IllegalArgumentException("Internal bug. Please file an issue on Github")
-                }
-            }
-
-    private fun onActionSideEffectFactory(
-        action: A,
-        stateAccessor: StateAccessor<S>
-    ): Flow<Action<S, A>> =
-        flow {
-            onActionBlock.invoke(
-                action,
-                stateAccessor,
-                {
-                    emit(
-                        SelfReducableAction<S, A>(
-                            loggingInfo = "Caused by on<$action>",
-                            reduce = it
-                        )
-                    )
-                }
-            )
-        }
-
-    override fun generateSideEffect(): SideEffect<S, Action<S, A>> {
-        return { actions: Flow<Action<S, A>>, state: StateAccessor<S> ->
-
-            actions
-                .filterStateAndUnwrapExternalAction(
-                    stateAccessor = state,
-                    subStateClass = subStateClass
-                )
-                .flatMapWithPolicy(flatMapPolicy) { action ->
-                    onActionSideEffectFactory(
-                        action = action,
-                        stateAccessor = state
-                    )
-                }
-        }
-    }
 }
 
 
