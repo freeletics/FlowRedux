5bee2b2eafb6f9e3b7461e5296d100ba94629a3b
Hannes <hannes.dorfmann@gmail.com>
2020-09-29 11:05:58 +0200

    Try again deploying website

--- deploy_website.sh
+++ deploy_website.sh
@@ -2,6 +2,10 @@ set -ex
 cp README.md docs/index.md
 rm -rf docs/Javadoc
 ./gradlew dokka
-mkdocs gh-deploy
-rm -rf docs/Javadoc
-rm -rf site
+mkdocs build
+git checkout gh-pages
+find . -mindepth 1 ! -regex '^./site' -delete
+cp -R site/ .
+git add .
+git commit -am "Releasing new version of docs"
+git push origin gh-pages
