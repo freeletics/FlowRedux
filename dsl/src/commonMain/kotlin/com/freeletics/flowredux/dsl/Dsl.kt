15f361e72ba9179c1f0447746d9f18e35d6d0208
Hannes <hannes.dorfmann@gmail.com>
2021-03-22 21:01:41 +0100

    isInState<Type> with additional condition

--- dsl/src/commonMain/kotlin/com/freeletics/flowredux/dsl/Dsl.kt
+++ dsl/src/commonMain/kotlin/com/freeletics/flowredux/dsl/Dsl.kt
@@ -50,12 +50,14 @@ class FlowReduxStoreBuilder<S : Any, A : Any> {
 
     /**
      * Define what happens if the store is in a certain state.
+     * "In a certain state" condition is true if state is instance of the type specified as generic function parameter.
      */
     inline fun <reified SubState : S> inState(
         noinline block: InStateBuilderBlock<SubState, S, A>.() -> Unit
     ) {
 
         // TODO check for duplicate inState { ... } blocks of the same SubType and throw Exception
+        //  or is this actaully a feature :)
         val builder = InStateBuilderBlock<SubState, S, A>(_isInState = { state ->
             SubState::class.isInstance(state)
         })
@@ -64,7 +66,27 @@ class FlowReduxStoreBuilder<S : Any, A : Any> {
     }
 
     /**
+     * This variation allows you to specify is a mix between inferring the condition of the generic function type
+     * and additionally can specify and ADDITIONAL condition that also must be true in addition to the check that
+     * the type as specified as generic fun parameter is an instance of the current state.
+     */
+    inline fun <reified SubState : S> inState(
+        noinline additionalIsInState: (SubState) -> Boolean,
+        noinline block: InStateBuilderBlock<SubState, S, A>.() -> Unit
+    ) {
+
+        // TODO check for duplicate inState { ... } blocks of the same SubType and throw Exception
+        //  or is this actaully a feature :)
+        val builder = InStateBuilderBlock<SubState, S, A>(_isInState = { state ->
+            SubState::class.isInstance(state) && additionalIsInState (state as SubState)
+        })
+        block(builder)
+        builderBlocks.add(builder as StoreWideBuilderBlock<S, A>)
+    }
+
+    /**
      * Define what happens if the store is in a certain state.
+     * @param isInState The condition under which we identify that the state machine is in a given "state".
      */
     fun inState(
         isInState: (S) -> Boolean,
