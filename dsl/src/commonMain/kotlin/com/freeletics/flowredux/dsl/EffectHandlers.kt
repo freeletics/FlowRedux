ae2d583d9327fb235fc80f00ef9b6aac18e144c7
Gabriel Ittner <gabriel@freeletics.com>
2021-09-27 22:14:10 +0200

    use fun interface for all handlers (#212)
    * use fun interface for all handlers
    
    * unused imports
    
    * Rename EffectHandlers.kt to Handlers.kt
    
    Co-authored-by: Hannes <hannes.dorfmann@gmail.com>

--- dsl/src/commonMain/kotlin/com/freeletics/flowredux/dsl/EffectHandlers.kt
+++
@@ -1,5 +0,0 @@
-package com.freeletics.flowredux.dsl
-
-typealias OnActionEffectHandler<InputState, A> = suspend (action: A, state: InputState) -> Unit
-typealias OnEnterStateEffectHandler<InputState> = suspend (state: InputState) -> Unit
-typealias CollectFlowEffectHandler<T, InputState> = suspend (value: T, state: InputState) -> Unit
