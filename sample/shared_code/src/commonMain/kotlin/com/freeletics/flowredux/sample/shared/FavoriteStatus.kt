0393f8f6ffc1a0de6dd38ffa58fa5928f0e130ab
Hannes Dorfmann <hannes.dorfmann@gmail.com>
2022-05-12 16:23:47 +0200

    Some more docs for sample

--- sample/shared_code/src/commonMain/kotlin/com/freeletics/flowredux/sample/shared/FavoriteStatus.kt
+++ sample/shared_code/src/commonMain/kotlin/com/freeletics/flowredux/sample/shared/FavoriteStatus.kt
@@ -1,8 +1,25 @@
 package com.freeletics.flowredux.sample.shared
 
 enum class FavoriteStatus {
+    /**
+     * It is not marked as favorite yet
+     */
     NOT_FAVORITE,
+
+    /**
+     * An operation (read: http request) is in progress to either mark it as favorite or not mark it
+     * as favorite
+     */
     OPERATION_IN_PROGRESS,
+
+    /**
+     * The operation (read: http request) to either mark it as favorite or not mark it
+     * as favorite has failed, so did not succeed.
+     */
     OPERATION_FAILED,
+
+    /**
+     * Marked as favorites
+     */
     FAVORITE
 }
