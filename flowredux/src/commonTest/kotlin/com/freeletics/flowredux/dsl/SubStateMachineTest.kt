3ca87d2375abc231228e04552e45e126c36fedaa
Josephine Richter <josephine.richter@freeletics.com>
2022-11-10 09:38:15 +0100

    add tests and adapt documentation

--- flowredux/src/commonTest/kotlin/com/freeletics/flowredux/dsl/SubStateMachineTest.kt
+++ flowredux/src/commonTest/kotlin/com/freeletics/flowredux/dsl/SubStateMachineTest.kt
@@ -34,7 +34,6 @@ class SubStateMachineTest {
         val receivedChildStateUpdatesParentState = mutableListOf<TestState>()
 
         val child = ChildStateMachine(initialState = TestState.S3) {
-
             inState<TestState.S3> {
                 on<TestAction.A1> { _, state ->
                     inS3onA1Action++
@@ -51,7 +50,6 @@ class SubStateMachineTest {
         }
 
         val parent = StateMachine {
-
             inState<TestState.Initial> {
                 onEnterStartStateMachine(
                     stateMachine = child,
@@ -96,7 +94,6 @@ class SubStateMachineTest {
     @Test
     fun `sub statemachine doesnt restart if state parent state is still the same`() =
         suspendTest {
-
             var factoryInvocations = 0
             var childEntersInitialState = 0
 
@@ -135,7 +132,6 @@ class SubStateMachineTest {
                 assertEquals(1, factoryInvocations)
                 assertEquals(1, childEntersInitialState)
 
-
                 sm.dispatchAsync(TestAction.A1)
                 assertEquals(TestState.GenericState("generic", 2), awaitItem())
                 assertEquals(1, factoryInvocations)
@@ -146,7 +142,6 @@ class SubStateMachineTest {
     @Test
     fun `sub statemachine factory is called every time parent state is entered`() =
         suspendTest {
-
             var factoryInvocations = 0
             var childEntersInitialState = 0
 
@@ -191,7 +186,6 @@ class SubStateMachineTest {
                 assertEquals(TestState.S2, awaitItem())
                 assertEquals(1, factoryInvocations)
                 assertEquals(1, childEntersInitialState)
-
 
                 sm.dispatchAsync(TestAction.A2)
 
@@ -241,7 +235,7 @@ class SubStateMachineTest {
 
                 // dispatch action to child statemachine
                 sm.dispatch(TestAction.A1)
-                delay(10) // give child a bit of time before continueing
+                delay(10) // give child a bit of time before continuing
                 assertEquals(childActionInvocations, 1)
 
                 // transition parent to other state
@@ -256,14 +250,78 @@ class SubStateMachineTest {
                     assertEquals(parentActionInvocations, i)
                     assertEquals(childActionInvocations, 1) // still 1, no change
                 }
+            }
+        }
 
+    @Test
+    fun `actions are only dispatched to sub statemachine if they are mapped`() =
+        suspendTest {
+            var childActionInvocations = 0
+            var parentActionInvocations = 0
+            val child = ChildStateMachine(initialState = TestState.S1) {
+                inState<TestState> {
+                    on<TestAction> { _, state ->
+                        childActionInvocations++
+                        state.noChange()
+                    }
+                }
+            }
+
+            val sm = StateMachine(initialState = TestState.S1) {
+                inState<TestState.S1> {
+                    onEnterStartStateMachine(
+                        stateMachine = child,
+                        actionMapper = {
+                            when (it) {
+                                TestAction.A1 -> it
+                                TestAction.A2 -> it
+                                TestAction.A3 -> null
+                                is TestAction.A4 -> null
+                            }
+                        }
+                    )
+                    on<TestAction> { _, state ->
+                        parentActionInvocations++
+                        state.noChange()
+                    }
+                }
+            }
+
+            sm.state.test {
+                // initial state
+                assertEquals(TestState.S1, awaitItem())
+
+                // dispatch mapped A1 action
+                sm.dispatch(TestAction.A1)
+                delay(10) // give child a bit of time before continuing
+                assertEquals(childActionInvocations, 1)
+                assertEquals(parentActionInvocations, 1)
+
+                // dispatch mapped A2 action
+                sm.dispatch(TestAction.A2)
+                delay(10) // give child a bit of time before continuing
+                assertEquals(childActionInvocations, 2)
+                assertEquals(parentActionInvocations, 2)
+
+                // dispatch unmapped A3 action
+                sm.dispatch(TestAction.A3)
+                delay(10) // give child a bit of time before continuing
+                // assert that child state machine was not triggered after first two initial actions
+                assertEquals(childActionInvocations, 2)
+                assertEquals(parentActionInvocations, 3)
+
+                // dispatch unmapped A4 action
+                sm.dispatch(TestAction.A4(0))
+                delay(10) // give child a bit of time before continuing
+                // assert that child state machine was not triggered after first two initial actions
+                assertEquals(childActionInvocations, 2)
+                assertEquals(parentActionInvocations, 4)
             }
         }
 
     @Test
     fun `reentering state so that substatemachine triggers works with same child instate`() =
         suspendTest {
-
             var childOnEnterS2 = 0
             var childActionA2 = 0
             var parentS2 = 0
@@ -314,16 +372,14 @@ class SubStateMachineTest {
                     assertEquals(i, childActionA2)
                     assertEquals(i, childOnEnterS2)
                 }
-
             }
-
         }
 }
 
 @OptIn(ExperimentalCoroutinesApi::class, FlowPreview::class)
 private fun ChildStateMachine(
     initialState: TestState = TestState.Initial,
-    builderBlock: FlowReduxStoreBuilder<TestState, TestAction>.() -> Unit,
+    builderBlock: FlowReduxStoreBuilder<TestState, TestAction>.() -> Unit
 ): FlowReduxStateMachine<TestState, TestAction> {
     return object : FlowReduxStateMachine<TestState, TestAction>(initialState) {
 
