181e18d9859bb4d3e39abc928e810cb918d23ebd
Hannes Dorfmann <sockeqwe@users.noreply.github.com>
2023-05-09 17:44:38 +0200

    Fix flaky on Test: actionBlockStopsWhenMovedToAnotherState (#487)
    * Use a real delay() on a real dispatcher to avoid flakiness
    
    * comment
    
    * Add acceptance criteria that cancelation must happen within 20ms
    
    * change acceptance criteria to 10 milliseconds
    
    * changed to  10ms

--- flowredux/src/commonTest/kotlin/com/freeletics/flowredux/dsl/OnActionTest.kt
+++ flowredux/src/commonTest/kotlin/com/freeletics/flowredux/dsl/OnActionTest.kt
@@ -3,6 +3,7 @@ package com.freeletics.flowredux.dsl
 import app.cash.turbine.awaitComplete
 import app.cash.turbine.awaitItem
 import app.cash.turbine.test
+import kotlinx.coroutines.Dispatchers
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertFalse
@@ -13,12 +14,17 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.test.advanceUntilIdle
 import kotlinx.coroutines.test.runCurrent
 import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.withContext
+import kotlin.time.DurationUnit
+import kotlin.time.ExperimentalTime
+import kotlin.time.measureTime
 
 @OptIn(ExperimentalCoroutinesApi::class)
 internal class OnActionTest {
 
+    @OptIn(ExperimentalTime::class)
     @Test
-    fun actionBlockStopsWhenMovedToAnotherState() = runTest {
+    fun actionBlockStopsWhenMovedToAnotherStateWithin10Milliseconds() = runTest {
         val signal = Channel<Unit>()
         val blockEntered = Channel<Boolean>()
 
@@ -28,8 +34,19 @@ internal class OnActionTest {
                 on<TestAction.A1> { _, state ->
                     blockEntered.send(true)
                     signal.awaitComplete()
-                    // while we wait for S2 to be emitted which cancels the block the cancelattion might happen slightly afterwards
-                    delay(100)
+                    // while we wait for S2 to be emitted which cancels this block
+                    // the cancellation of this block might happen a few milliseconds later.
+                    // Therefore we add a tiny delay and use a "real" dispatcher where delay would wait
+                    // for real (in contrast to TestDipsatcher used with runTest {...})
+                    // to avoid flakiness.
+                    withContext(Dispatchers.Default) {
+                        val timeElapsed = measureTime {
+                            // 10 ms should be enough to make sure that the cancellation happened in the meantime
+                            // because of state transition to TestState.S2 in on<TestAction.A2>.
+                            delay(10)
+                        }
+                        assertTrue(timeElapsed.toDouble(DurationUnit.MILLISECONDS) < 10, "Time Elapsed: $timeElapsed but expected to be < 10")
+                    }
                     // this should never be reached because state transition did happen in the meantime,
                     // therefore this whole block must be canceled
                     reached = true
@@ -49,8 +66,6 @@ internal class OnActionTest {
             sm.dispatchAsync(TestAction.A2)
             assertEquals(TestState.S2, awaitItem())
             signal.close()
-            advanceUntilIdle()
-            runCurrent()
         }
 
         assertFalse(reached)
