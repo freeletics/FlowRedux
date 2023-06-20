229a4bc711ed4f1e0dbed99c0def8c76ecb48d28
Gabriel Ittner <gabriel.ittner@gmail.com>
2023-06-20 13:18:58 +0200

    kdoc

--- flowredux/src/commonMain/kotlin/com/freeletics/flowredux/dsl/InStateBuilderBlock.kt
+++ flowredux/src/commonMain/kotlin/com/freeletics/flowredux/dsl/InStateBuilderBlock.kt
@@ -9,6 +9,10 @@ public class InStateBuilderBlock<InputState : S, S : Any, A : Any> internal cons
     override val isInState: SideEffectBuilder.IsInState<S>,
 ) : BaseBuilderBlock<InputState, S, A>() {
 
+    /**
+     * Allows handling certain actions or events only while an extra condition is `true`
+     * for the current state.
+     */
     public fun condition(
         condition: (InputState) -> Boolean,
         block: ConditionBuilderBlock<InputState, S, A>.() -> Unit,
@@ -19,6 +23,14 @@ public class InStateBuilderBlock<InputState : S, S : Any, A : Any> internal cons
         }.apply(block).sideEffectBuilders
     }
 
+    /**
+     * Anything inside this block will only run while the [identity] of the current state
+     * remains the same. The `identity` is determined by the given function and uses
+     * [equals] for the comparison.
+     *
+     * When the `identity` changes anything currently running in the block is cancelled.
+     * Afterwards a the blocks are started again for the new `identity`.
+     */
     public fun untilIdentityChanges(
         identity: (InputState) -> Any,
         block: IdentityBuilderBlock<InputState, S, A>.() -> Unit,
