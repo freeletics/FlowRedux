85bd7f28372b2f2a0bef51ff2923ae21c200fe8f
Gabriel Ittner <gabriel.ittner@gmail.com>
2023-12-15 09:20:35 +0100

    add failing test

--- flowredux/src/commonTest/kotlin/com/freeletics/flowredux/sideeffects/OnActionTest.kt
+++ flowredux/src/commonTest/kotlin/com/freeletics/flowredux/sideeffects/OnActionTest.kt
@@ -2,16 +2,21 @@ package com.freeletics.flowredux.sideeffects
 
 import app.cash.turbine.awaitItem
 import app.cash.turbine.test
+import app.cash.turbine.turbineScope
 import com.freeletics.flowredux.StateMachine
 import com.freeletics.flowredux.TestAction
 import com.freeletics.flowredux.TestState
+import com.freeletics.flowredux.dsl.ExecutionPolicy
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertIs
 import kotlin.test.assertTrue
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.awaitCancellation
 import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.test.runTest
 
 @OptIn(ExperimentalCoroutinesApi::class)
@@ -74,4 +79,33 @@ internal class OnActionTest {
             assertEquals(TestState.S2, awaitItem())
         }
     }
+
+
+    @Test
+    fun onActionOrdered() = runTest { turbineScope {
+        val sm = StateMachine(TestState.GenericNullableState(null, null)) {
+            inState<TestState.GenericNullableState> {
+                on<TestAction.A1>(executionPolicy = ExecutionPolicy.ORDERED) { _, state ->
+                    println("Received a1")
+                    state.mutate { copy(aString = "1") }
+                }
+
+                on<TestAction.A2>(executionPolicy = ExecutionPolicy.ORDERED) { _, state ->
+                    println("Received a2")
+                    state.mutate { copy(anInt = 2) }
+                }
+            }
+        }
+
+        val scope = CoroutineScope(context = Dispatchers.Unconfined)
+        val turbine = sm.state.testIn(scope)
+        scope.launch {
+            sm.dispatch(TestAction.A2)
+            sm.dispatch(TestAction.A1)
+        }
+
+        assertEquals(TestState.GenericNullableState(null, null), turbine.awaitItem())
+        assertEquals(TestState.GenericNullableState("1", null), turbine.awaitItem())
+        assertEquals(TestState.GenericNullableState("1", 2), turbine.awaitItem())
+    }}
 }
