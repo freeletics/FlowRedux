5eb365c1f7d204c6d3f444d216c008e61a7ba508
Gabriel Ittner <gabriel.ittner@gmail.com>
2024-09-30 10:58:22 +0200

    remove unused dependency

--- sample/android/sample-android.gradle.kts
+++ sample/android/sample-android.gradle.kts
@@ -13,7 +13,6 @@ freeletics {
 }
 
 dependencies {
-    implementation(libs.androidx.annotation)
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.recyclerview)
     implementation(libs.androidx.constraintlayout)
