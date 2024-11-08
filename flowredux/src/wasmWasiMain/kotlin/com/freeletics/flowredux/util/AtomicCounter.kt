3c768d988d1edfefa073782ae048eacc5ae09aed
Gabriel Ittner <gabriel.ittner@gmail.com>
2024-11-08 16:39:41 +0100

    add wasm/wasi AtomicCounter

---
+++ flowredux/src/wasmWasiMain/kotlin/com/freeletics/flowredux/util/AtomicCounter.kt
@@ -0,0 +1,11 @@
+package com.freeletics.flowredux.util
+
+internal actual class AtomicCounter actual constructor(initialValue: Int) {
+    private var atomicInt = initialValue
+
+    actual fun get(): Int = atomicInt
+
+    actual fun incrementAndGet(): Int = ++atomicInt
+
+    actual fun decrementAndGet(): Int = --atomicInt
+}
