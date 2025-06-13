edfa2dbb938548e085e66fd7ba7b4d244bc79259
Gabriel Ittner <gabriel.ittner@gmail.com>
2025-06-13 13:54:27 +0200

    remove deprecated methods (#829)
    * remove deprecated methods
    
    * remove obsolete test

--- flowredux/src/commonMain/kotlin/com/freeletics/flowredux2/FlowReduxStoreBuilder.kt
+++ flowredux/src/commonMain/kotlin/com/freeletics/flowredux2/FlowReduxStoreBuilder.kt
@@ -29,45 +29,4 @@ public class FlowReduxStoreBuilder<S : Any, A : Any> internal constructor() {
             isInState = { state -> subStateClass.isInstance(state) },
         ).apply(block).sideEffectBuilders
     }
-
-    /**
-     * This variation allows you to specify is a mix between inferring the condition of the generic function type
-     * and additionally can specify and ADDITIONAL condition that also must be true in addition to the check that
-     * the type as specified as generic fun parameter is an instance of the current state.
-     */
-    @Deprecated("use condition block inside inState instead")
-    public inline fun <reified SubState : S> inState(
-        noinline additionalIsInState: (SubState) -> Boolean,
-        noinline block: InStateBuilderBlock<SubState, S, A>.() -> Unit,
-    ) {
-        @Suppress("DEPRECATION")
-        inState(SubState::class, additionalIsInState, block)
-    }
-
-    @PublishedApi
-    @Deprecated("use condition block inside inState instead")
-    internal fun <SubState : S> inState(
-        subStateClass: KClass<SubState>,
-        additionalIsInState: (SubState) -> Boolean,
-        block: InStateBuilderBlock<SubState, S, A>.() -> Unit,
-    ) {
-        @Suppress("UNCHECKED_CAST")
-        sideEffectBuilderList += InStateBuilderBlock<SubState, S, A>(
-            isInState = { state -> subStateClass.isInstance(state) && additionalIsInState(state as SubState) },
-        ).apply(block).sideEffectBuilders
-    }
-
-    /**
-     * Define what happens if the store is in a certain state.
-     * @param isInState The condition under which we identify that the state machine is in a given "state".
-     */
-    @Deprecated("use condition block inside inState instead")
-    public fun inStateWithCondition(
-        isInState: (S) -> Boolean,
-        block: InStateBuilderBlock<S, S, A>.() -> Unit,
-    ) {
-        sideEffectBuilderList += InStateBuilderBlock<S, S, A>(
-            isInState = isInState,
-        ).apply(block).sideEffectBuilders
-    }
 }
