a711c010557708f8a28a663be7bd7f532027bcca
Gabriel Ittner <gabriel.ittner@gmail.com>
2025-06-23 10:15:54 +0200

    implement ExecutionPolicy.Throttled(Duration) (#840)
    * implement ExecutionPolicy.Throttled(Duration)
    
    * api dump
    
    * Update flowredux/src/commonMain/kotlin/com/freeletics/flowredux2/util/FlatMapWithExecutionPolicy.kt
    
    Co-authored-by: Kirill Boyarshinov <1917615+kboyarshinov@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Kirill Boyarshinov <1917615+kboyarshinov@users.noreply.github.com>

--- flowredux/src/commonMain/kotlin/com/freeletics/flowredux2/util/FlatMapWithExecutionPolicy.kt
+++ flowredux/src/commonMain/kotlin/com/freeletics/flowredux2/util/FlatMapWithExecutionPolicy.kt
@@ -1,8 +1,13 @@
 package com.freeletics.flowredux2.util
 
 import com.freeletics.flowredux2.ExecutionPolicy
+import kotlin.time.Duration
+import kotlin.time.TimeSource
+import kotlin.time.times
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.buffer
+import kotlinx.coroutines.flow.channelFlow
 import kotlinx.coroutines.flow.flatMapConcat
 import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.flatMapMerge
@@ -16,7 +21,29 @@ internal fun <T, R> Flow<T>.flatMapWithExecutionPolicy(
     transform: suspend (value: T) -> Flow<R>,
 ): Flow<R> =
     when (executionPolicy) {
-        ExecutionPolicy.CANCEL_PREVIOUS -> this.flatMapLatest(transform)
-        ExecutionPolicy.ORDERED -> this.flatMapConcat(transform)
-        ExecutionPolicy.UNORDERED -> this.flatMapMerge(transform = transform)
+        ExecutionPolicy.CancelPrevious -> this.flatMapLatest(transform)
+        ExecutionPolicy.Ordered -> this.flatMapConcat(transform)
+        ExecutionPolicy.Unordered -> this.flatMapMerge(transform = transform)
+        is ExecutionPolicy.Throttled ->
+            this
+                .throttleFirst(executionPolicy.duration, executionPolicy.timeSource)
+                .flatMapConcat(transform = transform)
     }
+
+private fun <T> Flow<T>.throttleFirst(windowDuration: Duration, timeSource: TimeSource): Flow<T> = channelFlow {
+    var windowStartTime = timeSource.markNow()
+    var emitted = false
+    collect { value ->
+        val delta = windowStartTime.elapsedNow()
+        if (delta >= windowDuration) {
+            windowStartTime += (delta / windowDuration).toInt() * windowDuration
+            emitted = false
+        }
+        if (!emitted) {
+            val result = trySend(value)
+            emitted = result.isSuccess
+        }
+    }
+}
+    // no buffer so that trySend will not send the value when the handler is still working
+    .buffer(0)
