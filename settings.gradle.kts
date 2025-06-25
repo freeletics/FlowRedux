1d134180ec2af7eca09c6818a7bbdaec19120a26
Gabriel Ittner <gabriel.ittner@gmail.com>
2025-06-25 13:18:31 +0200

    updates

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,7 +7,7 @@ pluginManagement {
 }
 
 plugins {
-    id("com.freeletics.gradle.settings").version("0.26.1")
+    id("com.freeletics.gradle.settings").version("0.27.0")
 }
 
 rootProject.name = "flowredux-library"
