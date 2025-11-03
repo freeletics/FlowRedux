b52d8bdd8ef364151b9516c94f0c63c9ed345335
Gabriel Ittner <gabriel.ittner@gmail.com>
2025-11-03 10:55:59 +0100

    simplify OnActionStartStateMachine implementation

--- flowredux/src/commonMain/kotlin/com/freeletics/flowredux2/sideeffects/OnActionStartStateMachine.kt
+++ flowredux/src/commonMain/kotlin/com/freeletics/flowredux2/sideeffects/OnActionStartStateMachine.kt
@@ -2,132 +2,78 @@ package com.freeletics.flowredux2.sideeffects
 
 import com.freeletics.flowredux2.ChangeableState
 import com.freeletics.flowredux2.ChangedState
-import com.freeletics.flowredux2.FlowReduxStateMachine
 import com.freeletics.flowredux2.FlowReduxStateMachineFactory
 import com.freeletics.flowredux2.State
 import kotlin.reflect.KClass
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.Job
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.channelFlow
-import kotlinx.coroutines.flow.onCompletion
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.job
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.sync.Mutex
-import kotlinx.coroutines.sync.withLock
 
 @ExperimentalCoroutinesApi
 internal class OnActionStartStateMachine<SubStateMachineState : Any, SubStateMachineAction : Any, InputState : S, TriggerAction : A, S : Any, A : Any>(
     override val isInState: IsInState<S>,
     private val stateMachineFactoryBuilder: State<InputState>.(action: TriggerAction) -> FlowReduxStateMachineFactory<SubStateMachineState, SubStateMachineAction>,
-    internal val subActionClass: KClass<out A>,
+    internal val subActionClass: KClass<TriggerAction>,
     private val actionMapper: (A) -> SubStateMachineAction?,
     private val handler: suspend ChangeableState<InputState>.(SubStateMachineState) -> ChangedState<S>,
 ) : ActionBasedSideEffect<InputState, S, A>() {
     override fun produceState(getState: GetState<S>): Flow<ChangedState<S>> {
         return channelFlow {
-            val subStateMachinesMap = SubStateMachinesMap<SubStateMachineState, SubStateMachineAction, TriggerAction>()
-            actions.collect { action ->
-                runOnlyIfInInputState(getState) { currentState ->
-                    // TODO take ExecutionPolicy into account
+            val actionBroadcast = MutableSharedFlow<A>()
+            actions
+                .collect { action ->
+                    // broadcast to all existing state machines
+                    actionBroadcast.emit(action)
+
+                    // see if we need to start a new state machine
                     if (subActionClass.isInstance(action)) {
-                        @Suppress("UNCHECKED_CAST")
-                        val actionThatStartsStateMachine = action as TriggerAction
+                        runOnlyIfInInputState(getState) { currentState ->
+                            val scope = CoroutineScope(coroutineContext + SupervisorJob(coroutineContext.job))
 
-                        val stateMachine = ChangeableState(currentState)
-                            .stateMachineFactoryBuilder(actionThatStartsStateMachine)
-                            .launchIn(this)
+                            @Suppress("UNCHECKED_CAST")
+                            val stateMachine = ChangeableState(currentState)
+                                .stateMachineFactoryBuilder(action as TriggerAction)
+                                .launchIn(scope)
 
-                        // Launch substatemachine
-                        val job = launch {
-                            stateMachine.state
-                                .onCompletion {
-                                    subStateMachinesMap.remove(stateMachine)
-                                }
-                                .collect { subStateMachineState ->
+                            val awaitActionCollection = Channel<Unit>()
+                            launch {
+                                actionBroadcast
+                                    .onStart { awaitActionCollection.send(Unit) }
+                                    .collect {
+                                        if (it == action) {
+                                            // cancel because same action was received again
+                                            scope.cancel()
+                                        } else if (!subActionClass.isInstance(it)) {
+                                            actionMapper(it)?.let {
+                                                stateMachine.dispatch(it)
+                                            }
+                                        }
+                                    }
+                            }
+                            // wait for the actionBroadcast to be collected to avoid following actions being dropped
+                            // due to a race condition between the launch starting and the collect here already handling
+                            // the next action
+                            awaitActionCollection.receive()
+
+                            launch {
+                                stateMachine.state.collect {
                                     runOnlyIfInInputState(getState) { parentState ->
-                                        val changedState = ChangeableState(parentState).handler(subStateMachineState)
+                                        val changedState = ChangeableState(parentState).handler(it)
                                         send(changedState)
                                     }
-                                }
-                        }
-                        subStateMachinesMap.cancelPreviousAndAddNew(
-                            actionThatStartedStateMachine = actionThatStartsStateMachine,
-                            stateMachine = stateMachine,
-                            job = job,
-                        )
-                    } else {
-                        // a regular action that needs to be forwarded
-                        // to the active sub state machine
-                        subStateMachinesMap.forEachStateMachine { stateMachine ->
-                            launch {
-                                actionMapper(action)?.let {
-                                    stateMachine.dispatch(it)
                                 }
                             }
                         }
                     }
                 }
-            }
-        }
-    }
-
-    internal class SubStateMachinesMap<S : Any, A : Any, ActionThatTriggeredStartingStateMachine : Any> {
-        internal data class StateMachineAndJob<S : Any, A : Any>(
-            internal val stateMachine: FlowReduxStateMachine<StateFlow<S>, A>,
-            internal val job: Job,
-        )
-
-        private val mutex = Mutex()
-        private val stateMachinesAndJobsMap = LinkedHashMap<ActionThatTriggeredStartingStateMachine, StateMachineAndJob<S, A>>()
-
-        suspend fun size(): Int = mutex.withLock { stateMachinesAndJobsMap.size }
-
-        suspend fun cancelPreviousAndAddNew(
-            actionThatStartedStateMachine: ActionThatTriggeredStartingStateMachine,
-            stateMachine: FlowReduxStateMachine<StateFlow<S>, A>,
-            job: Job,
-        ) {
-            mutex.withLock {
-                val existingStateMachinesAndJobs: StateMachineAndJob<S, A>? = stateMachinesAndJobsMap[actionThatStartedStateMachine]
-                existingStateMachinesAndJobs?.job?.cancel()
-
-                stateMachinesAndJobsMap[actionThatStartedStateMachine] = StateMachineAndJob(
-                    stateMachine = stateMachine,
-                    job = job,
-                )
-            }
-        }
-
-        suspend inline fun forEachStateMachine(
-            crossinline block: suspend (FlowReduxStateMachine<StateFlow<S>, A>) -> Unit,
-        ) {
-            mutex.withLock {
-                stateMachinesAndJobsMap.values.forEach { stateMachineAndJob ->
-                    block(stateMachineAndJob.stateMachine)
-                }
-            }
-        }
-
-        suspend fun remove(stateMachine: FlowReduxStateMachine<StateFlow<S>, A>): StateMachineAndJob<S, A>? {
-            // could be optimized for better runtime
-            val result = mutex.withLock {
-                var key: ActionThatTriggeredStartingStateMachine? = null
-                for ((actionThatTriggeredStarting, stateMachineAndJob) in stateMachinesAndJobsMap) {
-                    if (stateMachineAndJob.stateMachine === stateMachine) {
-                        key = actionThatTriggeredStarting
-                        break
-                    }
-                }
-
-                if (key != null) {
-                    stateMachinesAndJobsMap.remove(key)
-                } else {
-                    null
-                }
-            }
-
-            return result
         }
     }
 }
