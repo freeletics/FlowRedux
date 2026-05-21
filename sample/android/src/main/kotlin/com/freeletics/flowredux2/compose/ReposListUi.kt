5d1cb1bb1524fcc79df710599b9ee2d709bcdc3e
Kirill Boyarshinov <kirill@freeletics.com>
2026-05-21 15:42:36 +0200

    fix lint

--- sample/android/src/main/kotlin/com/freeletics/flowredux2/compose/ReposListUi.kt
+++ sample/android/src/main/kotlin/com/freeletics/flowredux2/compose/ReposListUi.kt
@@ -35,7 +35,7 @@ import com.freeletics.flowredux2.sample.shared.GithubRepository
 import com.freeletics.flowredux2.sample.shared.LoadNextPage
 import com.freeletics.flowredux2.sample.shared.RetryToggleFavoriteAction
 import com.freeletics.flowredux2.sample.shared.ToggleFavoriteAction
-import java.util.Locale
+import androidx.compose.ui.platform.LocalLocale
 import kotlinx.collections.immutable.ImmutableList
 import kotlinx.coroutines.flow.filter
 
@@ -181,7 +181,7 @@ private fun GithubRepoUi(
 
             Text(
                 modifier = Modifier.width(50.dp),
-                text = String.format(Locale.getDefault(), "%d", repo.stargazersCount),
+                text = String.format(LocalLocale.current.platformLocale, "%d", repo.stargazersCount),
             )
         }
     }
